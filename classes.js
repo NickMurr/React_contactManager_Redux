afebaa52066be9e045b46a3b8c41f83d605ac88b
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-11-28 17:35:21 +0200

    Git master

---
+++ classes.js
@@ -0,0 +1,22 @@
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+  getSummary() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  }
+}
+
+// Magazine SubClass
+class Magazine extends Book {
+  constructor(title, author, year, month) {
+    super(title, author, year);
+    this.month = month;
+  }
+}
+const mag1 = new Magazine('Moby Dick', 'john Longe', 2017, 'November');
+console.log(mag1);
+console.log(mag1.getSummary());
